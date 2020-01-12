fb21456835cf8a669c5ca4d80d0752aa71d96a43
aistomin <andrej.istomin.ikeen@gmail.com>
2020-01-12 16:42:15 +0100

    Issue #1. Application skeleton.

---
+++ src/main/java/com/github/aistomin/deutsche/post/api/Dummy.java
@@ -0,0 +1,7 @@
+package com.github.aistomin.deutsche.post.api;
+
+/**
+ * Created by aistomin on 12.01.20.
+ */
+public class Dummy {
+}
---
+++ src/test/java/com/github/aistomin/deutsche/post/api/DummyTest.java
@@ -0,0 +1,15 @@
+package com.github.aistomin.deutsche.post.api;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by aistomin on 12.01.20.
+ */
+class DummyTest {
+
+    @Test
+    void testDummy() {
+        Assertions.assertNotNull(new Dummy());
+    }
+}
