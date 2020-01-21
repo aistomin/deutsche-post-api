2865f3e8a736d2ef95110d7cacfa250e5be588b7
aistomin <andrej.istomin.ikeen@gmail.com>
2020-01-21 19:04:38 +0100

    Issue #5. Code coverage validation was added.

--- src/main/java/com/github/aistomin/deutsche/post/api/Dummy.java
+++ src/main/java/com/github/aistomin/deutsche/post/api/Dummy.java
@@ -2,6 +2,17 @@ package com.github.aistomin.deutsche.post.api;
 
 /**
  * Created by aistomin on 12.01.20.
+ * <p>
+ * Dummy class.
  */
 public class Dummy {
+
+    /**
+     * Dummy method.
+     *
+     * @return Dummy value.
+     */
+    String dummy() {
+        return "test";
+    }
 }
--- src/test/java/com/github/aistomin/deutsche/post/api/DummyTest.java
+++ src/test/java/com/github/aistomin/deutsche/post/api/DummyTest.java
@@ -5,11 +5,16 @@ import org.junit.jupiter.api.Test;
 
 /**
  * Created by aistomin on 12.01.20.
+ * <p>
+ * Test for {@link Dummy}
  */
 class DummyTest {
 
+    /**
+     * Dummy test method.
+     */
     @Test
     void testDummy() {
-        Assertions.assertNotNull(new Dummy());
+        Assertions.assertEquals("test", new Dummy().dummy());
     }
 }
